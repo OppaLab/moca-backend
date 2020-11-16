b3a6db17284ce68a4bfaa2bdf33ce4a803b662a1
JaewanHwang <jaewan9074@gmail.com>
2020-11-16 21:52:04 +0900

    [HOTFIX] key.properties 삭제

--- src/main/java/com/moca/springboot/SpringbootApplication.java
+++ src/main/java/com/moca/springboot/SpringbootApplication.java
@@ -2,12 +2,10 @@ package com.moca.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@PropertySource("key.properties")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
